743b53dc7ac689d153d77c6525f91cbc985f5f47
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2022-12-23 14:07:35 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-//your code here
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hour = now.getHours();
+
+    const hourDegrees = 30*hour + mins/2;
+    const minsDegrees = 6*mins;
+    const secondsDegrees = 6*seconds;
+    
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();

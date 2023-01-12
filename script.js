c01e0819df12d93b05e243e9c6bc5020d8e72091
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-12 13:03:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+
 function setDate() {
     const now = new Date();
     
